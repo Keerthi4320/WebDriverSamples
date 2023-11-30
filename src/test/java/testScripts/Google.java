4001e6e5f6c8794ce91656b5e33e7dcb3d530fc5
keerthi4320 <k.keerthivasan4320@gmail.com>
2023-11-30 06:46:30 +0000

    Inital Project with WebDriverSamples

---
+++ src/test/java/testScripts/Google.java
@@ -0,0 +1,19 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("Java Tutorial");
+		
+		
+	}
+
+}
